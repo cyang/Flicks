3e6a68b8b2c0dd1dcbdbcd090cfdaf766de92579
cyang001 <cyang001@citymail.cuny.edu>
2016-01-19 00:38:41 -0500

    Setup basic table view

---
+++ Flicks/MoviesViewController.swift
@@ -0,0 +1,55 @@
+//
+//  MoviesViewController.swift
+//  Flicks
+//
+//  Created by Christopher Yang on 1/18/16.
+//  Copyright © 2016 Christopher Yang. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self;
+        tableView.delegate = self;
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20;
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("movieCell", forIndexPath: indexPath);
+        
+        cell.textLabel!.text = "row \(indexPath.row)";
+        print("row \(indexPath.row)");
+
+        return cell;
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
