b3172298c66f0c36a4f474047626e421aa63d59d
cyang001 <cyang001@citymail.cuny.edu>
2016-01-19 03:08:29 -0500

    Retrieve poster images

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AFNetworking
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -45,9 +46,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row];
         let title = movie["title"] as! String;
         let overview = movie["overview"] as! String;
+        
+        let posterBaseUrl = "http://image.tmdb.org/t/p/w500";
+        let posterPath = movie["poster_path"] as! String;
+        let posterUrl = NSURL(string: posterBaseUrl + posterPath);
 
         cell.titleLabel.text = title;
         cell.overviewLabel.text = overview;
+        cell.posterView.setImageWithURL(posterUrl!);
         
         
         print("row \(indexPath.row)");
