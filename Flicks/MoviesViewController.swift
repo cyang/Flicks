7f2c404c20cdc3664ed287f6c83c9f2bdaabc3f8
cyang001 <cyang001@citymail.cuny.edu>
2016-01-19 02:26:48 -0500

    Retrieve movie titles into table view cells

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -11,13 +11,16 @@ import UIKit
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var movies: [NSDictionary]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self;
         tableView.delegate = self;
 
-        // Do any additional setup after loading the view.
+        getNowPlayingData();
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,7 +29,11 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20;
+        if let movies = movies {
+            return movies.count;
+        } else {
+            return 0;
+        }
     }
     
     // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
@@ -35,10 +42,38 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("movieCell", forIndexPath: indexPath);
         
-        cell.textLabel!.text = "row \(indexPath.row)";
+        let movie = movies![indexPath.row];
+        let title = movie["title"] as! String;
+        
+        cell.textLabel!.text = title;
         print("row \(indexPath.row)");
 
         return cell;
+    }
+    
+    func getNowPlayingData(){
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                            
+                            self.movies = responseDictionary["results"] as? [NSDictionary];
+                            self.tableView.reloadData();
+                    }
+                }
+        });
+        task.resume()
     }
     
 
